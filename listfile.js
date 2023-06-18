d200ccb905f5ecb4429301095196296649575b60
anshu sharma <anshusharma6327@gmail.com>
2023-06-19 00:12:28 +0530

    remove app from device / uninstall apps

--- listfile.js
+++ listfile.js
@@ -24,19 +24,13 @@ const listInstalledApps = async () => {
     if (!devices.includes("device")) {
       throw new Error("No device connected or unauthorized");
     }
-
+    const packageName = "com.google.android.apps.magazines"
     // List all installed apps using ADB
     const packageList = await executeADBCommand("adb shell pm list packages -3");
-    const packages = packageList
-      .split("\n")
-      .filter((line) => line.startsWith("package:"))
-      .map((line) => line.replace("package:", ""));
-
-    console.log("Installed apps:");
-    packages.forEach((packageName) => {
-      console.log(packageName);
-    });
-
+    const uninstallOutput = await executeADBCommand(`adb uninstall ${packageName}`);
+    console.log("Uninstalling app...", uninstallOutput);
+    console.log("List of installed apps:");
+    console.log(packageList);
     console.log("App listing completed.");
   } catch (error) {
     console.error("An error occurred:", error);
